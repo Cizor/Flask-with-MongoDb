c7946d7003eeafe5a5e8741b389a072a4aced49a
Amrit Srivastava <yourcizor@gmail.com>
2017-06-16 19:44:57 +0530

    MongoDb integration
    Tested some strings.
    Was able to find them in MongoDB and got their values.
    Showed File name and their values in HTML template

--- Flask/Hello.py
+++ Flask/Hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
@@ -32,6 +33,37 @@ def login():
 	else:
 		user = request.args.get('nm')
 		return redirect(url_for('success',name = user))
+
+#APIs to use HTML rendering from Python code
+@app.route('/hardhtml/')
+def render_html():
+	return '<html><body><h1>I am HTML Renderered from Python</h1></body></html>'
+
+@app.route('/templatehtml/')
+def template_html():
+	return render_template('first_hello.html')
+
+@app.route('/usertemplate/<user>/')
+def user_html(user):
+	return render_template('first_hello.html',name = user)
+
+@app.route('/mongo/')
+def mongo_test():
+	client = MongoClient() #default localhost 27017
+	pass_html = list()
+	db = client['database_ui1']
+	posts = db.posts
+	strId = "[" + "STATUS_MESSAGE_WILL_BE_DELETED_24_HOURS" + "]"
+	string_posts = posts.find({},{strId:1,'File':1})
+
+	print string_posts
+
+	for i in string_posts:
+		if strId in i:
+			pass_html.append(i)
+	print pass_html
+	return render_template('condition.html', result = pass_html)
+
 
 #Decorator to add URL Rules
 #app.add_url_rule('/hello/<name>','hello',hello_world)
